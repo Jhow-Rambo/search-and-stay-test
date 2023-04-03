32b97da0b7419d55d3c66ec81641f49b8fe9647e
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-04-02 22:55:06 -0300

    feat: create rules page with authentication

---
+++ pages/rules.vue
@@ -0,0 +1,18 @@
+<template>
+  <Rules />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'RulesPage',
+  middleware({ route, redirect }) {
+    const token =
+      typeof window !== 'undefined' ? localStorage.getItem('token') : null
+    if (!token && route.path !== '/') {
+      redirect('/')
+    }
+  },
+})
+</script>
